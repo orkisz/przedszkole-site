9dda1c077315c38975443d064879107d8193af45
Krzysztof Orkisz <krzysiek.orkisz@gmail.com>
2021-03-21 22:50:27 +0100

    one item opened by default

--- src/layouts/partials/accordion/accordion-item.jsx
+++ src/layouts/partials/accordion/accordion-item.jsx
@@ -1,48 +1,32 @@
-import * as React from 'react';
+import React from 'react';
 import * as styles from './accordion-item.module.scss';
 
-class AccordionItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggleIsOpen = this.toggleIsOpen.bind(this);
-    this.state = {
-      opened: false
-    };
+const AccordionItem = ({ children, opened, onClick }) => {
+  const [title, content] = React.Children.toArray(children);
+  let accordionItemClassName = styles.accordionItem;
+  if (opened) {
+    accordionItemClassName += ` ${styles.accordionItemOpened}`;
   }
-
-  toggleIsOpen() {
-    this.setState(state => ({
-      opened: !state.opened
-    }));
-  }
-
-  render() {
-    const [title, content] = React.Children.toArray(this.props.children);
-    let accordionItemClassName = styles.accordionItem;
-    if (this.state.opened) {
-      accordionItemClassName += ` ${styles.accordionItemOpened}`;
-    }
-    return (
-            <li>
-              <div className={accordionItemClassName}
-                   onClick={this.toggleIsOpen}>
-                <div className={styles['accordionItem__line']}>
-                  <div className={styles['accordionItem__title']}>
-                    {title}
-                  </div>
-                  <span className={styles['accordionItem__icon']}/>
+  return (
+          <li>
+            <div className={accordionItemClassName}
+                 onClick={onClick}>
+              <div className={styles['accordionItem__line']}>
+                <div className={styles['accordionItem__title']}>
+                  {title}
                 </div>
-                <div className={styles['accordionItem__inner']}>
-                  <div className={styles['accordionItem__content']}>
-                    <p className={styles['accordionItem__paragraph']}>
-                      {content}
-                    </p>
+                <span className={styles['accordionItem__icon']}/>
+              </div>
+              <div className={styles['accordionItem__inner']}>
+                <div className={styles['accordionItem__content']}>
+                  <div className={styles['accordionItem__paragraph']}>
+                    {content}
                   </div>
                 </div>
               </div>
-            </li>
-    )
-  }
+            </div>
+          </li>
+  );
 }
 
 export default AccordionItem;
--- src/layouts/partials/accordion/accordion.jsx
+++ src/layouts/partials/accordion/accordion.jsx
@@ -1,19 +1,19 @@
-import * as React from 'react';
 import AccordionItem from './accordion-item';
+import React, { cloneElement, useState } from 'react';
 
-class Accordion extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const items = React.Children.toArray(children).filter(item => item.type === AccordionItem);
-    return (
-            <ul>
-              {items}
-            </ul>
-    );
-  }
+const Accordion = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const items = React.Children.toArray(children).filter(item => item.type === AccordionItem);
+  return (
+          <ul>
+            {items.map((item, index) => {
+              return cloneElement(item, {
+                opened: activeIndex === index,
+                onClick: () => setActiveIndex(index)
+              })
+            })}
+          </ul>
+  );
 };
 
 export default Accordion;
