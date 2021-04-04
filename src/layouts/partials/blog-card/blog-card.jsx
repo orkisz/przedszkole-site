ab5a8d3cc9547ed9d75f7b91ffd727aa125b8b9d
Krzysztof Orkisz <krzysiek.orkisz@gmail.com>
2021-04-04 18:09:21 +0200

    fixed height of blog card

--- src/layouts/partials/blog-card/blog-card.jsx
+++ src/layouts/partials/blog-card/blog-card.jsx
@@ -5,13 +5,13 @@ import * as styles from './blog-card.module.scss';
 const BlogCard = ({ node }) => {
   let heading = (node.html || '').replace(/(<([^>]+)>)/gi, '');
   const spaceIdx = heading.substr(0, 200).lastIndexOf(' ');
-  heading = heading.substring(0, spaceIdx).trim() + '...';
+  heading = heading.substring(0, spaceIdx).replace(/\W+$/, '').trim() + '...';
 
   const formattedDate = new Date(node.frontmatter.date).toLocaleDateString();
 
   return (
           <div className="column is-4">
-            <div className="card">
+            <div className={`${styles.card} card`}>
               {node.frontmatter.image &&
               <div className="card-image">
                 <figure className="image is-4by3">
@@ -22,11 +22,11 @@ const BlogCard = ({ node }) => {
                       dateTime={node.frontmatter.date}>{formattedDate}</time>
               </div>
               }
-              <div className={`${styles.contentWrapper} card-content`}>
+              <div className={`card-content`}>
                 <h4 className={`${styles.title} my-3`}
                     role="link"
                     onClick={() => navigate(node.fields.slug)}>{node.frontmatter.title}</h4>
-                <div className="content mb-5">
+                <div className="content mb-6">
                   <p>{heading}</p>
                 </div>
                 <Link className={styles.link}
