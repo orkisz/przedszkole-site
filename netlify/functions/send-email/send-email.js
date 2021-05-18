2a2614f896e6a73482cc9b1cdec7d156a5437106
Krzysztof Orkisz <krzysiek.orkisz@gmail.com>
2021-05-18 09:04:36 +0200

    move nodemailer to main package.json

--- netlify/functions/send-email/send-email.js
+++ netlify/functions/send-email/send-email.js
@@ -1,4 +1,3 @@
-// with thanks to https://github.com/Urigo/graphql-modules/blob/8cb2fd7d9938a856f83e4eee2081384533771904/website/lambda/contact.js
 const process = require('process');
 const { validateEmail, validateLength } = require('./validations');
 const nodemailer = require('nodemailer');
