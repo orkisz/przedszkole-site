04710ee8098107055716f635e38a6c2281cab045
Krzysztof Orkisz <krzysiek.orkisz@gmail.com>
2021-05-19 09:23:49 +0200

    personnel finished

--- src/pages/about/personnel.jsx
+++ src/pages/about/personnel.jsx
@@ -34,7 +34,7 @@ const Personnel = () => {
                                      const { description, name, role, photo } = person;
                                      const even = index % 2 !== 0;
                                      const colorIndex = index % 3;
-                                     const textClass = photo ? 'is-7-desktop is-full-touch' : 'is-8-desktop is-offset-2-desktop is-full-touch';
+                                     const textClass = photo ? 'is-7-desktop' : 'is-8-desktop is-offset-2-desktop';
                                      const text = (
                                              <div className={`column ${textClass}`}>
                                                <h4><p className="mb-0">{role}</p></h4>
@@ -45,12 +45,14 @@ const Personnel = () => {
                                              </div>
                                      );
                                      const img = photo && (
-                                             <div className={`column is-5-desktop ${styles.imageColumn}`}>
-                                               <figure className={`image is-1by1`}>
-                                                 <img className={`is-rounded ${styles.image} ${styles[colors[colorIndex]]}`}
-                                                      src={applyTransform(photo[0], TRANSFORMATION_PERSONNEL_1BY1)}
-                                                      alt={name}/>
-                                               </figure>
+                                             <div className={`column is-5-desktop`}>
+                                               <div className={styles.imageWrap}>
+                                                 <figure className={`image is-1by1`}>
+                                                   <img className={`is-rounded ${styles.image} ${styles[colors[colorIndex]]}`}
+                                                        src={applyTransform(photo[0], TRANSFORMATION_PERSONNEL_1BY1)}
+                                                        alt={name}/>
+                                                 </figure>
+                                               </div>
                                              </div>
                                      );
                                      let elements = [text, img];
@@ -60,7 +62,7 @@ const Personnel = () => {
 
                                      return (
                                              <div key={name}
-                                                  className="columns is-variable is-8 my-6">
+                                                  className={`columns is-variable is-8 my-6 ${even ? styles.columns : styles.columnsReverse}`}>
                                                {elements}
                                              </div>
                                      )
