3c3b9c84b0febd64e8d06be1ec30380591f3ccdc
Krzysztof Orkisz <krzysztof.orkisz@softwareone.com>
2021-09-02 21:44:00 +0200

    new groups structure and packages update

--- src/layouts/partials/groups-accordion.jsx
+++ src/layouts/partials/groups-accordion.jsx
@@ -1,6 +1,6 @@
 import { graphql, StaticQuery } from 'gatsby';
 import React from 'react';
-import { getFirstParagraph, stripHtmlTags } from '../../utils/html';
+import { fixOrphans, getFirstParagraph, stripHtmlTags } from '../../utils/html';
 import Accordion from './accordion/accordion';
 import AccordionItem from './accordion/accordion-item';
 import * as styles from './groups-accordion.module.scss';
@@ -8,36 +8,30 @@ import * as styles from './groups-accordion.module.scss';
 const GroupsAccordion = () => {
   return (
           <StaticQuery query={graphql`
-          query GroupsQuery {
-            allMarkdownRemark(
-              filter: {fields: {category: {eq: "group"}}}
-              sort: {fields: frontmatter___order}
-              limit: 3
-            ) {
-              edges {
-                node {
-                  frontmatter {
-                    name
+            query GroupsQuery {
+              groups {
+                fields {
+                  groupFormatted {
+                    body
                     image
+                    photo
+                    name
                   }
-                  html
-                  id
                 }
               }
             }
-          }
           `}
                        render={data => (
                                <Accordion className={`${styles.accordion} mb-6`}>
-                                 {data.allMarkdownRemark.edges.map((edge, index) => {
-                                   const firstPara = stripHtmlTags(getFirstParagraph(edge.node.html));
+                                 {data.groups.fields.groupFormatted.slice(0, 3).map((group, index) => {
+                                   const firstPara = fixOrphans(stripHtmlTags(getFirstParagraph(group.body)));
                                    return (
-                                           <AccordionItem key={edge.node.id}
-                                                          title={edge.node.frontmatter.name}
-                                                          image={edge.node.frontmatter.image[0]}
+                                           <AccordionItem key={group.name}
+                                                          title={group.name}
+                                                          image={group.image[0]}
                                                           headClassName={styles[`accordionItem${index + 1}`]}
                                                           contentClassName={styles.accordionContent}>
-                                             {firstPara}
+                                             <span dangerouslySetInnerHTML={{ __html: firstPara }}/>
                                            </AccordionItem>
                                    )
                                  })}
